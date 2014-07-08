4f5f58454002fa90d74eabec03780fd5582186ea
BestKora <developer@bestkora.com>
2014-07-09 00:41:25 +0400

    Initial Commit

---
+++ Assinment-1-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assinment-1-Swift
+//
+//  Created by Tatiana Kornilova on 7/9/14.
+//  Copyright (c) 2014 Tatiana Kornilova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
